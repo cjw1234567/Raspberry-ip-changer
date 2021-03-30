e47d98550e085989cbbb247b7d7fc0951bfcc479
Mintlover <sdfhfv65@gmail.com>
2021-03-30 09:40:18 +0900

    끝

--- ipchanger.py
+++ ipchanger.py
@@ -10,19 +10,16 @@ def change_static_ip(ip_address):
     ethIndex = 0
     conf_file = '/etc/dhcpcd.conf'
        
-    # Sanitize/validate params above
     with open(conf_file, 'r') as file:
         data = file.readlines()
 
-    # Find if config exists
     ethFound = next((x for x in data if 'interface eth0' in x), None)
 
     if ethFound:
         ethIndex = data.index(ethFound)
         if data[ethIndex].startswith('#'):
-            data[ethIndex].replace('#', '') # commented out by default, make active
+            data[ethIndex].replace('#', '') 
 
-    # If config is found, use index to edit the lines you need ( the next 3)
     if ethIndex:
         data[ethIndex+1] = f'static ip_address={ip_address}/24\n'
 
